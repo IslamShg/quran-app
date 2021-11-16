6c8a1d18dbd89702a38f15f76c416f0bcb3f27b8
Islam Shagaew <islamshg@mail.ru>
2021-11-16 23:24:35 +0300

    unused hook import removed

--- src/components/TafsirBlock/TafsirBlock.jsx
+++ src/components/TafsirBlock/TafsirBlock.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import DOMPurify from 'dompurify'
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace'
